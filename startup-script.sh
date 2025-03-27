b62016c12af03d56d210b9e874f7d45ced3c9a94
Halite Lati The Snep <e-lucska@iskule.no>
2025-03-27 10:13:45 +0100

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# Make directory for this stuff
+mkdir /app
+cd /app
+
+# Update and install stuff
 sudo apt-get update
 sudo apt install git nodejs npm -y
+
+# Get discbro
 git clone https://github.com/LukeNotAndrew/spill
-cd /home/alterra/spill
+cd /app/spill
+
+# Install packages, run
 npm i
 sudo node .
